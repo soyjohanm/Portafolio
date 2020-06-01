bd144c1e1a18373714ca862ffa756806548ce821
Johan Solano <johanmsolanoc@gmail.com>
2020-05-31 23:25:46 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang='es' dir='ltr'>
+  <head>
+    <meta charset='utf-8'>
+    <meta http-equiv='Cache-Control' content='max-age=31563000'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <meta name='HandheldFriendly' content='true'>
+    <meta name='description' content='Mi portafolio.'>
+    <meta name='msapplication-TileColor' content='#ffffff'>
+    <meta name='msapplication-TileImage' content='ms-icon-144x144.png'>
+    <meta name='theme-color' content='#ffffff'>
+    <link rel='canonical' href='https://johansolano.com.ve/'>
+    <link rel='apple-touch-icon' sizes='180x180' href='apple-icon-180x180.png'>
+    <link rel='icon' type='image/png' sizes='32x32' href='favicon-32x32.png'>
+    <link rel='icon' type='image/png' sizes='16x16' href='favicon-16x16.png'>
+    <link rel="stylesheet" href="./style.css">
+    <link rel='manifest' href='./manifest.json'>
+    <base href='./'>
+    <title>Johan Solano - Portafolio</title>
+    <!-- <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="preload" as="font" crossorigin> -->
+    <!--
+	  <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.7/fullpage.min.js" integrity="sha256-e13jRNqOX98m6UAwI/yZTpcDseJtA8s86yfFs4Sqrv8=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+	  -->
+    <script type="text/javascript" src="./fullpage.js"></script>
+    <script type="text/javascript" src="./typed.js"></script>
+    <script type="text/javascript" src="./materialize.js"></script>
+  </head>
+  <body>
+    <main>
+      <?php require (dirname (__FILE__)).'/body.php'; ?>
+    </main>
+  </body>
+</html>
